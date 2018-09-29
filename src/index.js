6b0409bec0db21ee61a7b22c03103d016c457741
vladimirDobrik <vladimir.d.6kg@gmail.com>
2018-09-29 20:44:12 +0300

    zeros/fix: implement getzeroscount func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function getZerosCount(number) {
-  // your implementation
-}
+  module.exports = function getZerosCount(number) {
+      let count = 0;
+      
+      for (let i = 1; i <= number; i++) {
+          let temp = i;
+          
+          while (temp % 5 === 0) {
+              temp /= 5;
+              count++;
+          }
+      }
+      
+      return count;
+  }
